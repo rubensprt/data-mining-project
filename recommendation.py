b0a23be7d2dafd28d7ea36f964575dc74c4c6d16
Nelson <dutzhaoyeyu@163.com>
2016-11-26 00:00:31 +0800

    create recommendation.py

---
+++ recommendation.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+"""
+This py file will realize two recommendation systems(item-based/user-based)\
+using datasets in our local directory.
+Why don't we upload our datasets into GitHub? Because the size limitation for
+a file in GitHub is only 100 size!
+"""
+
+import pandas as pd
+from pandas import Series, DataFrame
